d77f13f5a6976abff85d8fdd2d2ccd8a20fd5800
NoahNtabo23 <135032672+NoahNtabo23@users.noreply.github.com>
2023-06-13 20:51:30 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(a){
+  let answer;
+  if(a<=400){
+    return 'This one is on me !'
+  }else if(a>2000 && a<2500){
+    return 'I will gladly take your thirty bucks.'
+  }else{
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(a){
+  let result;
+  if(a=='NYC'){
+    return 'Ok, sounds good'
+  }else{
+    return 'No go.'
+  }
+}
+  
+
+
+function switchOnCharmFromTip(a){
+  let answer;
+  switch(a){
+    case 'generous':
+      return 'Thank you so much.'
+      break;
+    case 'not as generous':
+      return 'Thank you.'
+      break;
+    case 'thanks for everything':
+      return 'Bye.'
+     
+  }
+
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
